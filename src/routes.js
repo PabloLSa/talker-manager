0b176be63c58c8e3abe8eabfeb6a3429205e3d29
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-04-22 16:02:49 -0300

    Req2
     Criando o endpoint GET /talker/:id

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,12 @@ const routers = express.Router();
 
 routers.get('/', async (_req, res) => res.status(200).json(await dataGet(path)));
 
+routers.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await dataGet(path);
+  const talker = talkers.find((tk) => tk.id === +id);
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(200).json(talker);
+});
+
 module.exports = routers;
